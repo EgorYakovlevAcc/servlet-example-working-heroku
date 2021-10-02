06908ef1f836dc9791a4eefe255ccf7002bc9301
EgorYakovlevAcc <yakovlev.es@phystech.edu>
2021-10-02 10:23:15 +0300

    logger

--- src/main/java/org/example/WelcomeServlet.java
+++ src/main/java/org/example/WelcomeServlet.java
@@ -5,8 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WelcomeServlet extends HttpServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WelcomeServlet.class);
 
     @Override
     public void doGet(HttpServletRequest request,
@@ -15,5 +18,6 @@ public class WelcomeServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         out.println("Hello, application!");
+        LOGGER.log(Level.INFO, "Welcome page has been uploaded successfully");
     }
 }
